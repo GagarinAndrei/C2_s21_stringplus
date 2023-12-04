2b458c78069e7a6db64971ec87d1320baf5f48f0
Dawn Kali <gagarin1982@gmail.com>
2023-12-04 12:13:03 +0300

    dawnkali_test: dndfndvn

--- src/tests/test_me.c
+++ src/tests/test_me.c
@@ -24,7 +24,7 @@ int main(void) {
                               test_sprintf_c(),  //*
                               test_sprintf_d(),  //*
                               // test_sprintf_e(),
-                             test_sprintf_f(),  //*
+                        //     test_sprintf_f(),  //*
                               // // test_sprintf_g(),
                        //       test_sprintf_hex1(),
                        //        test_sprintf_hex2(),
@@ -33,7 +33,7 @@ int main(void) {
                       //        test_sprintf_octal(),
                        //       test_sprintf_percent(),
                        //       test_sprintf_pointer(),
-                              test_sprintf_string(),    //*
+                        //      test_sprintf_string(),    //*
                         //      test_sprintf_unsigned(),  //*
                               NULL};
 
